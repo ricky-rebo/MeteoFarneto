c6f22f03510222132d96a0f8e0849bce6517f43f
Riccardo Rebottini <ricky@rebottini.it>
2020-10-23 10:40:45 +0200

    aggiunta pagina '/glosario'

---
+++ src/pages/glossario.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { MDBMedia } from 'mdbreact';
+import glossary from "../../content/glossary_data.json";
+import Layout from '../components/layout';
+
+const GlossaryPage = () => {
+    return (
+        <Layout>
+            <MDBMedia list className="mt-3">
+                {
+                    glossary.map(item => (
+                        <MDBMedia key={item.name} tag="li" className="mb-2 mt-2">
+                            <MDBMedia body>
+                                <MDBMedia heading> { item.name } </MDBMedia>
+                                { item.description }
+                                <hr />
+                            </MDBMedia>
+                        </MDBMedia>
+                    ))
+                }
+            </MDBMedia>
+        </Layout>
+            
+    )
+}
+
+export default GlossaryPage;
