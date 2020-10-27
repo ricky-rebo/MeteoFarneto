161d4da7cc464e8e2a26a9dc60a87b2f20060f35
Riccardo Rebottini <ricky@rebottini.it>
2020-10-27 21:37:35 +0100

    common stuff moved in src/shared folder

---
+++ src/shared/externals.js
@@ -0,0 +1,10 @@
+const siteUrl = "https://rebottini.it"
+
+
+export const realtimeDataUrl = siteUrl + "";
+export const aqiDataUrl      = "";
+
+
+export const webcamUrl             = siteUrl + "/webcam/webcamimage.jpg"
+export const timelapsTodayUrl      = siteUrl + "";
+export const timelapseYesterdayUrl = siteUrl + "";
